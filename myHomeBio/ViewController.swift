b5675b69aa2d206e4d536494a40b0501e79c4c09
Jason McCoy <brodooski@gmail.com>
2016-07-07 23:51:28 -0700

    Initial Commit

---
+++ myHomeBio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myHomeBio
+//
+//  Created by Jason McCoy on 7/7/16.
+//  Copyright © 2016 Jason McCoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
